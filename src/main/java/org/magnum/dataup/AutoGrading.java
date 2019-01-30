4fd44621ac56ff0ab11c38a6adf614b777acbddf
Jules White <jules.white@gmail.com>
2019-01-30 14:35:13 -0600

    Initial Release

---
+++ src/main/java/org/magnum/dataup/AutoGrading.java
@@ -0,0 +1,67 @@
+/*
+ * 
+ * Copyright 2014 Jules White
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ */
+package org.magnum.dataup;
+
+/**
+ * This class allows you to estimate the score for your solution before handing it in.
+ * 
+ * This class also generates the required submission package to hand your solution in.
+ * 
+ * In order to run this AutoGrading application, in Eclipse, Right-click on it->Run As->Java Application
+ * 
+ * 
+ * Please read the instructions that the application prints very carefully. It will store the generated
+ * solution submission packages in the coursera-submission folder within the project directory. After running
+ * this application, you will need to right-click on the root of the project in Eclipse->Refresh to see this 
+ * folder inside of Eclipse. Each time that you run this application, it generates a new unique submission
+ * package. Make sure that you submit the correct (e.g., most up to date) package when you turn your assignment
+ * in.
+ * 
+ * @author jules
+ *
+ *                       DO NOT MODIFY THIS CLASS
+                    ___                    ___           ___                            
+     _____         /\  \                  /\  \         /\  \                           
+    /::\  \       /::\  \                 \:\  \       /::\  \         ___              
+   /:/\:\  \     /:/\:\  \                 \:\  \     /:/\:\  \       /\__\             
+  /:/  \:\__\   /:/  \:\  \            _____\:\  \   /:/  \:\  \     /:/  /             
+ /:/__/ \:|__| /:/__/ \:\__\          /::::::::\__\ /:/__/ \:\__\   /:/__/              
+ \:\  \ /:/  / \:\  \ /:/  /          \:\~~\~~\/__/ \:\  \ /:/  /  /::\  \              
+  \:\  /:/  /   \:\  /:/  /            \:\  \        \:\  /:/  /  /:/\:\  \             
+   \:\/:/  /     \:\/:/  /              \:\  \        \:\/:/  /   \/__\:\  \            
+    \::/  /       \::/  /                \:\__\        \::/  /         \:\__\           
+     \/__/         \/__/                  \/__/         \/__/           \/__/           
+      ___           ___                                     ___                         
+     /\  \         /\  \         _____                     /\__\                        
+    |::\  \       /::\  \       /::\  \       ___         /:/ _/_         ___           
+    |:|:\  \     /:/\:\  \     /:/\:\  \     /\__\       /:/ /\__\       /|  |          
+  __|:|\:\  \   /:/  \:\  \   /:/  \:\__\   /:/__/      /:/ /:/  /      |:|  |          
+ /::::|_\:\__\ /:/__/ \:\__\ /:/__/ \:|__| /::\  \     /:/_/:/  /       |:|  |          
+ \:\~~\  \/__/ \:\  \ /:/  / \:\  \ /:/  / \/\:\  \__  \:\/:/  /      __|:|__|          
+  \:\  \        \:\  /:/  /   \:\  /:/  /   ~~\:\/\__\  \::/__/      /::::\  \          
+   \:\  \        \:\/:/  /     \:\/:/  /       \::/  /   \:\  \      ~~~~\:\  \         
+    \:\__\        \::/  /       \::/  /        /:/  /     \:\__\          \:\__\        
+     \/__/         \/__/         \/__/         \/__/       \/__/           \/__/        
+*/
+public class AutoGrading {
+
+	public static void main(String[] args) throws Exception {
+		AutoGradingSpec.grade(args);
+	}
+
+}
