f1377904916f9be663d1c16986def93eba14704b
Sagar Bhatt <49220046+Sagar1087@users.noreply.github.com>
2020-09-05 15:45:58 -0600

    Create @Service class to implement the API.
    Use @Atuowired to automatically get object of Service class. Generate Get and Post Method to receive and send Video Metadata.

--- src/main/java/org/magnum/dataup/ControllerClass.java
+++ src/main/java/org/magnum/dataup/ControllerClass.java
@@ -1,117 +1,71 @@
 /*
- * 
+ *
  * Copyright 2014 Jules White
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- * 
+ *
  */
 package org.magnum.dataup;
 
 import org.magnum.dataup.model.Video;
-import org.magnum.dataup.model.VideoStatus;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.context.request.RequestContextHolder;
-import org.springframework.web.context.request.ServletRequestAttributes;
-import retrofit.client.Response;
-import retrofit.http.Body;
-import retrofit.mime.TypedFile;
 
-import javax.servlet.http.HttpServletRequest;
-import java.util.*;
-import java.util.concurrent.atomic.AtomicLong;
+import java.util.Collection;
 
-@Controller
-public class ControllerClass{
+@RestController
+public class ControllerClass {
 
-	/**
-	 * You will need to create one or more Spring controllers to fulfill the
-	 * requirements of the assignment. If you use this file, please rename it
-	 * to something other than "AnEmptyController"
-	 * 
-	 * 
-		 ________  ________  ________  ________          ___       ___  ___  ________  ___  __       
-		|\   ____\|\   __  \|\   __  \|\   ___ \        |\  \     |\  \|\  \|\   ____\|\  \|\  \     
-		\ \  \___|\ \  \|\  \ \  \|\  \ \  \_|\ \       \ \  \    \ \  \\\  \ \  \___|\ \  \/  /|_   
-		 \ \  \  __\ \  \\\  \ \  \\\  \ \  \ \\ \       \ \  \    \ \  \\\  \ \  \    \ \   ___  \  
-		  \ \  \|\  \ \  \\\  \ \  \\\  \ \  \_\\ \       \ \  \____\ \  \\\  \ \  \____\ \  \\ \  \ 
-		   \ \_______\ \_______\ \_______\ \_______\       \ \_______\ \_______\ \_______\ \__\\ \__\
-		    \|_______|\|_______|\|_______|\|_______|        \|_______|\|_______|\|_______|\|__| \|__|
-                                                                                                                                                                                                                                                                        
-	 * 
-	 */
+    /**
+     * You will need to create one or more Spring controllers to fulfill the
+     * requirements of the assignment. If you use this file, please rename it
+     * to something other than "AnEmptyController"
+     * <p>
+     * <p>
+     * ________  ________  ________  ________          ___       ___  ___  ________  ___  __
+     * |\   ____\|\   __  \|\   __  \|\   ___ \        |\  \     |\  \|\  \|\   ____\|\  \|\  \
+     * \ \  \___|\ \  \|\  \ \  \|\  \ \  \_|\ \       \ \  \    \ \  \\\  \ \  \___|\ \  \/  /|_
+     * \ \  \  __\ \  \\\  \ \  \\\  \ \  \ \\ \       \ \  \    \ \  \\\  \ \  \    \ \   ___  \
+     * \ \  \|\  \ \  \\\  \ \  \\\  \ \  \_\\ \       \ \  \____\ \  \\\  \ \  \____\ \  \\ \  \
+     * \ \_______\ \_______\ \_______\ \_______\       \ \_______\ \_______\ \_______\ \__\\ \__\
+     * \|_______|\|_______|\|_______|\|_______|        \|_______|\|_______|\|_______|\|__| \|__|
+     */
 
-	private String getDataUrl(long videoId){
-		String url = getUrlBaseForLocalServer() + "/video/" + videoId + "/data";
-		return url;
-	}
+    @Autowired
+    VideoService videoService;
 
-	private String getUrlBaseForLocalServer() {
-		HttpServletRequest request =
-				((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getRequest();
-		String base =
-				"http://"+request.getServerName()
-						+ ((request.getServerPort() != 80) ? ":"+request.getServerPort() : "");
-		return base;
-	}
-
-	private static final AtomicLong currentId = new AtomicLong(0L);
-
-	private Map<Long,Video> videos = new HashMap<Long, Video>();
-
-	public Video save(Video entity) {
-		checkAndSetId(entity);
-		videos.put(entity.getId(), entity);
-		return entity;
-	}
-
-	private void checkAndSetId(Video entity) {
-		if(entity.getId() == 0){
-			entity.setId(currentId.incrementAndGet());
-		}
-	}
-
-	@RequestMapping(value = "/video", method = RequestMethod.GET)
-	@ResponseBody
-	public Collection<Video> getVideoList() {
-
-		List<Video> videoList = new ArrayList<Video>();
-
-		for(Long id : videos.keySet()){
-			videoList.add(videos.get(id));
-		}
-		return videoList;
-	}
+    @RequestMapping(value = "/video", method = RequestMethod.POST,
+			consumes = {MediaType.APPLICATION_JSON_VALUE},
+			produces ={MediaType.APPLICATION_JSON_VALUE})
+    public ResponseEntity<Video> postVideo(@RequestBody Video v) {
+        ResponseEntity<Video> respVideo = new ResponseEntity<>(videoService.postVideo(v), HttpStatus.OK);
+        return respVideo;
+    }
 
 
-	@RequestMapping(value = "/video",method = RequestMethod.POST)
-	@ResponseBody
-	public Video addVideo(@RequestBody Video v) {
+    @ResponseBody
+    @RequestMapping(value = "/video",method = RequestMethod.GET,
+            produces = {MediaType.APPLICATION_JSON_VALUE})
+    public ResponseEntity<Collection<Video>> getVideoList(){
+        ResponseEntity<Collection<Video>> videoList = new ResponseEntity<>(videoService.getVideoList(), HttpStatus.OK);
+        return videoList;
+    }
 
-		Video video1 = new Video();
-		video1.setId(v.getId());
-		video1.setTitle(v.getTitle());
-		video1.setDuration(v.getDuration());
-		video1.setContentType(v.getContentType());
-		//video1.setLocation(v.getLocation());
-		video1.setSubject(v.getSubject());
-		video1.setDataUrl(getDataUrl(video1.getId()));
-		video1 = save(video1);
 
-		System.out.println(videos.get(video1.getId()) + " " + videos.keySet());
 
-		return video1;
-	}
 
 }
---
+++ src/main/java/org/magnum/dataup/VideoService.java
@@ -0,0 +1,79 @@
+package org.magnum.dataup;
+
+import org.magnum.dataup.model.Video;
+import org.springframework.stereotype.Service;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.*;
+import java.util.concurrent.atomic.AtomicLong;
+
+@Service
+public class VideoService {
+
+    static final AtomicLong currentID = new AtomicLong(0L);
+    Map<Long,Video> videoMaps = new HashMap<Long, Video>();
+    List<Video> videoLists = new ArrayList<Video>();
+
+    /**
+     * 1. Create videoID by calling SetId() method. [ID should not be 0].
+     * 2. Create a Map<Long Id,Video v>. Put video object along with ID as a key inside Map.
+     * 3. Generate the full URL by passing ID.
+     * 4. Create a collection class and store video object inside the collection.
+     * 5. Return video object.
+     * @param video
+     * @return
+     */
+    public Video postVideo(Video video){
+        checkAndSetId(video);
+        videoMaps.put(video.getId(),video);
+        String dataURL = getDataUrl(video.getId());
+        video.setDataUrl(dataURL);
+
+        videoLists.add(video);
+        return video;
+    }
+
+    /**
+     * Impl step 1. Method to creat ID for video.
+     */
+    public void checkAndSetId(Video entity) {
+        if(entity.getId() == 0){
+            entity.setId(currentID.incrementAndGet());
+        }
+    }
+
+    /**
+     * Impl Step 3. Pass videoID as an args. Return VideoURL as a String Param.
+     * @param videoId
+     * @return
+     */
+    public String getDataUrl(long videoId){
+        String url = getUrlBaseForLocalServer() + "/video/" + videoId + "/data";
+        return url;
+    }
+
+    private String getUrlBaseForLocalServer() {
+        HttpServletRequest request =
+                ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getRequest();
+        String base =
+                "http://"+request.getServerName()
+                        + ((request.getServerPort() != 80) ? ":"+request.getServerPort() : "");
+        return base;
+    }
+
+    //============================================================================
+    //============================================================================
+
+    /**
+     * 6. Return Collection of video.
+     * @return
+     */
+    public Collection<Video> getVideoList(){
+        return videoLists;
+    }
+
+
+
+}
